1efe7650f98d218615e054ec872ff77746bd3430
wlsgur073 <wlsgur073@gmail.com>
2023-11-30 00:15:27 +0900

    회원 도메인 실행과 테스트 추가

--- core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,6 +2,7 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService {
 
+    // 현재 MemberServiceImpl는 추상화인 MemberRepository와 구현체인 MemoryMemberRepository 모두 의존하고 있다.
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
     @Override
