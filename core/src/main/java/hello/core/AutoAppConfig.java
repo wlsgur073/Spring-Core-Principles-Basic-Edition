591544f987e6525dac00b5bd5cae658d774b0646
wlsgur073 <wlsgur073@gmail.com>
2024-01-20 21:22:55 +0900

    @ComponentScan의 필터에 대한 commit

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -9,6 +9,11 @@ import org.springframework.context.annotation.FilterType;
         // Configuration이 붙는 것들을 빼주겠다고 설정함.
         // 왜냐하면 @Configuration 안을 보면 @Component 설정이 되어 있기에 그것들도 자동등록 돼 버린다.
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+        // xml에서 설정해주는 것과 동일하다. 베이스 패키지를 설정해서 그 아래 모든 것들에 영향을 주게 설정할 수 있다.
+        // 패키지 위치는 보통 패키지를 지정하지 않고 최상단에 두는 편이다.
+        // @SpringBootApplication 을 프로젝트 시작 루트 위치에 두는 것이 관례이다.
+        // 이 어노테이션에 컴포넌트 스캔이 들어있기 때문이다. 따라서 ComponentScan를 굳이 쓸 필요 없다.
+        , basePackages = "hello.core.member"
 )
 public class AutoAppConfig {
 
