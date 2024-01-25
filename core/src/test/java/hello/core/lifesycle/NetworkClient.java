11708546e8d08abd5e9e7eab58947267182dc997
wlsgur073 <wlsgur073@gmail.com>
2024-01-25 22:32:21 +0900

    인터페이스 InitializingBean, DisposableBean

--- core/src/test/java/hello/core/lifesycle/NetworkClient.java
+++ core/src/test/java/hello/core/lifesycle/NetworkClient.java
@@ -1,13 +1,19 @@
 package hello.core.lifesycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
+
+    /*
+    * InitializingBean, DisposableBean 는 스프링 전용 인터페이스이기에
+    * 해당 코드가 스프링 전용 인터페이스에 의존한다. 더 좋은 방법이 있기에 지금은 사용하지 않는다.
+    * */
 
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메세지");
     }
 
     public void setUrl(String url) {
@@ -26,5 +32,20 @@ public class NetworkClient {
     // 서비스 종료 시 호출
     public void disconnect() {
         System.out.println("close : " + url);
+    }
+
+
+    // 의존관계 주입이 끝나면 호출하는 메서드
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메세지");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+        disconnect();
     }
 }
